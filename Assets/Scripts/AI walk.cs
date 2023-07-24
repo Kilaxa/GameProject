db1adf73c0dad303e13dad3413c5e6897932c0dc
CherePapa <pen.aaa@mail.ru>
2023-07-24 17:44:43 +0400

    враг и скрипт для движения врага

---
+++ Assets/Scripts/AI walk.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public Transform Character;
+    public float speed = 0.1F;
+    private Vector3 directionOfCharacter;
+    private bool challenged = false;
+    
+    void Update()
+    {
+        if (challenged)
+        {
+            directionOfCharacter = Character.transform.position - transform.position;
+            directionOfCharacter = directionOfCharacter.normalized;
+            transform.Translate (directionOfCharacter * speed, Space.World);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            challenged = true;
+        }
+    }
+}
