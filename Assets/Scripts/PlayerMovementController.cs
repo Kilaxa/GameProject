ca2995e19568467736daeeb2b51cbb7fa318e23c
Даня <grigoriewdania@gmail.com>
2023-07-24 19:54:34 +0500

    Новая система прыжков
    +имитация гравитации с character controller

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,30 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
 
-    public float moveSpeed;
+    public float moveSpeed = 6f;
     public float turnSmoothTime = 0.1f;
-
+    private Vector3 direction;
     float turnSmoothVelocity;
+
+    private Vector3 velocity;
+    public float jumpHeight = 10.0f;
+    public float gravityValue = -9.81f;
+
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        MoveController();
+        JumpController();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-        if(direction.magnitude >= 0.1f)
+    public void OnMove(InputValue value)
+    {
+        float horizontal = value.Get<Vector2>().x;
+        float vertical = value.Get<Vector2>().y;
+        direction = new Vector3 (horizontal, 0f, vertical);
+    }
+
+    private void MoveController()
+    {
+        if (direction.magnitude >= 0.1f)
         {
             float turnAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, turnAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, turnAngle , 0f) * Vector3.forward;
+            Vector3 moveDirection = Quaternion.Euler(0f, turnAngle, 0f) * Vector3.forward;
+
             controller.Move(moveDirection * moveSpeed * Time.deltaTime);
         }
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if(value.isPressed && controller.isGrounded)
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
+    }
+
+    private void JumpController()
+    {
+        velocity.y += gravityValue * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
