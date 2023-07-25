bfe91bc5e9901a93ccf30c34d0ead134b6b14de1
Даня <grigoriewdania@gmail.com>
2023-07-25 21:45:00 +0500

    основа бега

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -9,8 +9,12 @@ public class PlayerMovementController : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Animator animator;
 
-    public float moveSpeed = 6f;
+    public float moveSpeed = 3f;
+    private float moveSpeedT;
+    public float runSpeed = 6f;
+
     public float turnSmoothTime = 0.1f;
     private Vector3 direction;
     float turnSmoothVelocity;
@@ -19,6 +23,10 @@ public class PlayerMovementController : MonoBehaviour
     public float jumpHeight = 10.0f;
     public float gravityValue = -9.81f;
 
+    private void Start()
+    {
+        moveSpeedT = moveSpeed;
+    }
     void Update()
     {
         MoveController();
@@ -36,6 +44,8 @@ public class PlayerMovementController : MonoBehaviour
     {
         if (direction.magnitude >= 0.1f)
         {
+            animator.SetBool("isWalking", true);
+
             float turnAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, turnAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -44,11 +54,14 @@ public class PlayerMovementController : MonoBehaviour
 
             controller.Move(moveDirection * moveSpeed * Time.deltaTime);
         }
+        else
+            animator.SetBool("isWalking", false);
     }
 
     public void OnJump(InputValue value)
     {
-        if(value.isPressed && controller.isGrounded)
+        animator.SetTrigger("isJumping");
+        if (value.isPressed && controller.isGrounded)
         velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
     }
 
@@ -56,5 +69,26 @@ public class PlayerMovementController : MonoBehaviour
     {
         velocity.y += gravityValue * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    public bool pressedOnce = false;        
+    private void OnRun(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            if (pressedOnce && animator.GetBool("isWalking"))
+            {
+                pressedOnce = false;
+                animator.SetBool("isRunning", true);
+                moveSpeed = runSpeed;
+            }
+            else
+            {
+                pressedOnce = true;
+                animator.SetBool("isRunning", false);
+                moveSpeed = moveSpeedT;
+            }
+        }
+  
     }
 }
