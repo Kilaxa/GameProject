fca3afe8d2a8a0143b23440d7bab0b08d37bb1f4
Даня <grigoriewdania@gmail.com>
2023-07-26 01:48:52 +0500

    какашка

--- Assets/Scripts/AI walk.cs
+++ Assets/Scripts/AI walk.cs
@@ -1,74 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    public float seeDistans = 5f;
-    public float attackDistanse = 2f;
-    public float speed = 6f;
-    private GameObject _player;
-    Animator _myAnimation;
-    private float _attackType;
-    private float _maxValue = 3f;
-    private float _minValue = 0f;
-
-    public enum AttackType
-    {
-        Ax,
-        Bow,
-        Sword
-    }
-
+    public Transform Character;
+    public float speed = 0.1F;
+    private Vector3 directionOfCharacter;
+    private bool challenged = false;
+    Animator myAnimation;
 
     private void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        _myAnimation.SetBool("Walk", true);
+        myAnimation = GetComponent<Animator>();
     }
 
     void Update()
     {
-           
-        
-           _attackType = Time.time + UnityEngine.Random.Range(_minValue, _maxValue);
-        
+        if (challenged)
+        {
+            directionOfCharacter = Character.transform.position - transform.position;
+            directionOfCharacter = directionOfCharacter.normalized;
+            transform.Translate (directionOfCharacter * speed, Space.World);
+            myAnimation.SetBool("Walk", true); 
+        }
+    }
 
-        if(Vector3.Distance (transform.position,_player.transform.position) < seeDistans)
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-            if (Vector3.Distance (transform.position, _player.transform.position) > attackDistanse)
-            {
-                RandomStyleAttack(_attackType);
-                transform.LookAt (_player.transform);
-                transform.Translate (new Vector3 (0,0,speed * Time.deltaTime));
-            }
+            challenged = true;
         }
-        else { }
-    }
-    void Awake()
-    {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        _myAnimation = GetComponent<Animator>();
-    }
-    void RandomStyleAttack(float _attackType)
-    {
-        switch (_attackType)
-        {
-            case (float)AttackType.Ax:
-                break;
-            case (float)AttackType.Bow:
-                break;
-            case (2):
-                SwordAttack();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
-    void SwordAttack()
-    {
-        //вызов анимации и сама атака
-        Debug.Log("sword attack");
     }
 }
